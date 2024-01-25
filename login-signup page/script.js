67c614420fa6ff986ad5caa610ef29e47c5b19cc
sanskarsingh_iitbhu21 <97400210+SanskarSinghiit@users.noreply.github.com>
2024-01-25 23:37:04 +0530

    Add files via upload

---
+++ login-signup page/script.js
@@ -0,0 +1,17 @@
+/* script.js */
+
+function togglePassword() {
+    var passwordInput = document.getElementById('typePasswordX-2');
+    var toggleIcon = document.getElementById('toggleIcon');
+  
+    if (passwordInput.type === 'password') {
+      passwordInput.type = 'text';
+      toggleIcon.classList.remove('fa-eye');
+      toggleIcon.classList.add('fa-eye-slash');
+    } else {
+      passwordInput.type = 'password';
+      toggleIcon.classList.remove('fa-eye-slash');
+      toggleIcon.classList.add('fa-eye');
+    }
+  }
+  
